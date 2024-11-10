d0d39ad0f5a97d59c7049262ff600224c94fdca5
Paolo-Daniel-B-A <pbenaventea@unsa.edu.pe>
2024-11-09 22:49:14 -0500

    Se probo que countMemoryEntries devuelve el numero de direcciones en el heap de distintas variables

--- ejemplo.c
+++ ejemplo.c
@@ -6,7 +6,10 @@ void display(){
   //char** blackSquare = reverse(whiteSquare);
   //interpreter(blackSquare);
   int *a;
+  int *b;
   memoryAlloc((void **)&a, sizeof(int));
+  memoryAlloc((void **)&b, sizeof(int));
+
   countMemoryEntries();
   unregisterPointer((void **)&a);
   countMemoryEntries();
