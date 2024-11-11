39730e8b565dfd0f2382cbab4bf04895b8b80f6a
Alexander Huayhua <ahuayhuap@unsa.edu.pe>
2024-11-10 19:44:49 -0500

    se probo la función unregisterpointer()para desvincular las entradas de memoria de la matriz 2x2, funciono correctamente
    Esperabamos que con este modelo reducido, apareciera el problema del ejemplo original, pero aparentemente todo el proceso de unlink memory funciona perfectamente

--- ejemplo.c
+++ ejemplo.c
@@ -5,11 +5,20 @@
 void display(){
   //char** blackSquare = reverse(whiteSquare);
   //interpreter(blackSquare);
+  //asignar
   int **a;
   memoryAlloc((void **)&a, 2*sizeof(int)); 
   countMemoryEntries();
   for (int i = 0; i<2; i++)
     memoryAlloc((void**)&a[i], 2*sizeof(int));
+  countMemoryEntries();
+  
+  //desvincular memoria
+  for(int i=0;i<2;i++){
+    unregisterPointer((void**)&a[i]);
+  }
+  countMemoryEntries();
+  unregisterPointer((void**)&a);
   countMemoryEntries();
   garbageCollector();
 }
