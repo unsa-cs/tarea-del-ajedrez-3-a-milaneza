6653f860dafc5552dfbfe593f89c715af009afb0
Alexander Huayhua <ahuayhuap@unsa.edu.pe>
2024-11-11 22:01:18 -0500

    ya que nuestros experimentos con la desvinculación manual no daban error,pensamos hacer una copia directa de la funcion UnlinkMemory(), y probarla simultaneamente con la desvinculación manual, inesperadamente el error del ejemplo aparece al usar unlinkMemory()
    se hizo una copia de unlinkMemory() ya que esta no esta definida en chess.h . Hicimos el cambio despues de hacerle seguimiento a los ejemplos anteriores con gdb sin exito

--- ejemplo.c
+++ ejemplo.c
@@ -1,36 +1,36 @@
 #include "chess.h"
 #include "figures.h"
 #include "gc.h"
+void unlinkMemory_2(char** fig){
+  countMemoryEntries();
+  for(int i = 0; fig[i]; i++)
+    unregisterPointer((void**)&fig[i]);
+  countMemoryEntries();
+  unregisterPointer((void**)&fig);
+  countMemoryEntries();
+}
+
 
 void display(){
   //char** blackSquare = reverse(whiteSquare);
   //interpreter(blackSquare);
   //asignar
-  /*
-  int rows=58; 
+  int rows=10; 
   int cols=rows;
-  int **a;
-  memoryAlloc((void **)&a, (rows+1)*sizeof(int*)); 
+  char **a;
+  memoryAlloc((void **)&a, (rows+1)*sizeof(char*)); 
   countMemoryEntries();
   for (int i = 0; i<rows; i++)
-    memoryAlloc((void**)&a[i], (cols+1)*sizeof(int));
+    memoryAlloc((void**)&a[i], (cols+1)*sizeof(char));
+  /*
   countMemoryEntries();
-  
   //desvincular memoria
   for(int i=0;a[i];i++){
     unregisterPointer((void**)&a[i]);
   }
   countMemoryEntries();
   unregisterPointer((void**)&a);
-  countMemoryEntries();
-  garbageCollector();
-  */
-  int *a;
-  memoryAlloc((void **)&a, sizeof(int));
-  registerPointerToMemory((void **)&a, a);
-  //registerPointerToMemory((void **)&a, a);
-  countMemoryEntries();
-  unregisterPointer((void **)&a);
-  countMemoryEntries();
+  countMemoryEntries();*/
+  unlinkMemory_2(a);
   garbageCollector();
 }
